440717e6c4970a96a3356f77a1f074ad3ef23e39
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-14 14:05:08 -0400

    Think it's all in working order now.

--- index.js
+++ index.js
@@ -87,17 +87,26 @@ server.get('/hash', (req, res) => {
   // // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
 });
 
-function protected() {
-  const password = headers.password;
-  const username = headers.username;
+function protected (req, res, next) {
+  const { username, password } = req.headers;
 
   if (username && password) {
-    const hash = bcrypt.hashSync(password, 8);
-    res.status(200).json({ hash })
-  } else {
-    res.status(400).json({ message: 'Please enter valid credentials' })
-  }
-}
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+    } else {
+        res.status(400).json({ message: 'Please provide valid credentials' })
+    }
+  };
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
